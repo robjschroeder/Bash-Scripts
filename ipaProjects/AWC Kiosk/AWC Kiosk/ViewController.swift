6d14d4ae60618cc85eae0166ed4671a51a5472b0
Robert Schroeder <robjschroeder@gmail.com>
2020-12-11 22:34:22 -0700

    Add files via upload

---
+++ ipaProjects/AWC Kiosk/AWC Kiosk/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AWC Kiosk
+//
+//  Created by Robert J Schroeder on 8/12/19.
+//  Copyright © 2019 Robert J Schroeder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func SSRPM(_ sender: Any) {
+    }
+    
+    
+    @IBAction func Request(_ sender: Any) {
+    }
+    
+}
+
